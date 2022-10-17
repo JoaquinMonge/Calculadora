725c29aa0534050240ae8ed13321046025f2fd2e
JoaquinMonge <joamonb@gmail.com>
2022-10-17 15:26:49 -0600

    first commit

---
+++ src/OperationButton.jsx
@@ -0,0 +1,13 @@
+import { ACTION } from "./App";
+
+export const OperationButton = ({ dispatch, operation }) => {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTION.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+};
